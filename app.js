4b4000cd9ccd3f48b69cf443fcfa4a867078cf0c
Michael Ashton <mykl.ashton@gmail.com>
2017-07-06 10:48:44 -0400

    push up them initial files

---
+++ app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use('/static', express.static('static'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/static/index.html");
+})
+
+// put routes here
+//    ok
+
+
+
+app.listen(3000, function () {
+    console.log('Express running on http://localhost:3000/.')
+});
